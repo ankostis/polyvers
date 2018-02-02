a5f592ad6de778f124a9d77bd65d21cac3ea451a
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-02-02 09:34:49 +0200

    chore(lic): fix some lic-headers on sources

--- multivers/autoinstance_traitlet.py
+++ multivers/autoinstance_traitlet.py
@@ -1,4 +1,10 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015-2018 European Commission (JRC);
+# Licensed under the EUPL 1.2+ (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+
 """AutoInstance traitlet creates an instance from config-params."""
 
 from traitlets import TraitError, Any, Instance, This
--- tests/test_multivers.py
+++ tests/test_multivers.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright 2015-2018 European Commission (JRC);
+# Licensed under the EUPL 1.2+ (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 
 """Tests for `multivers` package."""
 
