9dae043d0b978b4471efcf7cf215e65f1e2654e3
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-02-14 20:05:50 +0100

    fix(mpump): delete duplicate `chain_cmd()`

--- polyvers/mainpump.py
+++ polyvers/mainpump.py
@@ -9,46 +9,6 @@
 """Utils pumping results out of yielding functions for `main()`."""
 
 
-def chain_cmds(app_classes, argv=None, **root_kwds):
-    """
-    Instantiate(optionally) and run a list of ``[cmd, subcmd, ...]``, linking each one as child of its predecessor.
-
-    :param app_classes:
-        A list of cmd-classes: ``[root, sub1, sub2, app]``
-        Note: you have to "know" the correct nesting-order of the commands ;-)
-    :param argv:
-        cmdline args passed to the root (1st) cmd only.
-        Make sure they do not contain any sub-cmds.
-        Like :meth:`initialize()`, if undefined, replaced with ``sys.argv[1:]``.
-    :return:
-        The root(1st) cmd to invoke :meth:`Aplication.start()`
-        and possibly apply the :func:`pump_cmd()` on its results.
-
-    - Normally `argv` contain any sub-commands, and it is enough to invoke
-      ``initialize(argv)`` on the root cmd.  This function shortcuts
-      arg-parsing for subcmds with explict cmd-chaining in code.
-    - This functions is the 1st half of :meth:`Cmd.launch_instance()`.
-    """
-    if not app_classes:
-        raise ValueError("No cmds to chained passed in!")
-
-    app_classes = list(app_classes)
-    root = app = None
-    for app_cl in app_classes:
-        if type(app_cl).__name__ != 'Application':
-                    raise ValueError("Expected an Application-class instance, got %r!" % app_cl)
-        if not root:
-            ## The 1st cmd is always orphan, and gets returned.
-            root = app = app_cl(**root_kwds)
-        else:
-            app.subapp = app = app_cl(parent=app)
-        app.initialize(argv)
-
-    app_classes[0]._instance = app
-
-    return root
-
-
 class ConsumerBase:
     """Checks if all boolean items (if any) are True, to decide final bool state."""
     any_none_bool = False
