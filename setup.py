87154cad946417885c2661f681b761628855f9f2
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-02-20 15:04:50 +0100

    fix(build): wheel had 1st-level packages only
    ...and `pip install .[test]` also had same problem.
    + Caused bc `find_package(include=...)`
    includes just that!

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     author="Kostis Anagnostopoulos",
     author_email='ankostis@gmail.com',
     url='https://github.com/jrcstu/polyvers',
-    packages=find_packages(include=['polyvers']),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=requirements,
     license='EUPL 1.2',
