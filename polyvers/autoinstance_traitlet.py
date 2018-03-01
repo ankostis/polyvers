13903152b42d16da55c1618574d16e7caea48ed8
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-03-01 23:46:53 +0200

    refact(autotrait): cast dicts-->Configurables also when in defaults

--- polyvers/autoinstance_traitlet.py
+++ polyvers/autoinstance_traitlet.py
@@ -7,26 +7,37 @@
 
 """AutoInstance traitlet creates an instance from config-params."""
 
-from ._vendor.traitlets.traitlets import TraitError, Instance
+from ._vendor.traitlets.traitlets import Instance
 from ._vendor.traitlets.config.configurable import Configurable
 
 
 class AutoInstance(Instance):
+    _cast_types = dict
+
+    def cast(self, value):
+        iclass = self.klass
+        if (issubclass(iclass, Configurable) and isinstance(value, dict)):
+            value = iclass(**value)
+            ## Mark it, to set `parent` when known (on `validate()`).
+            value._auto_instanciated_dict = value
+        else:
+            value = iclass(value)
+
+        return value
+
     def validate(self, obj, value):
         iclass = self.klass
         if isinstance(value, iclass):
             return value
-        elif not (issubclass(iclass, Configurable) and
-                  isinstance(value, dict)):
-            self.error(obj, value)  # Report original error.
+        elif (issubclass(iclass, Configurable) and isinstance(value, dict)):
+            value = iclass(parent=obj, **value)
         else:
-            ## Auto-create configurables.
-            #
-            try:
-                value = iclass(parent=obj, **value)
-#                 # Ensure element's value has highest priority as config.
-#                 value.udate_config(self._prefix_element_params(iclass.__name__, value))
-            except TraitError as _:
-                self.error(obj, value)  # Report original error.
+            values_dict = getattr(value, '_auto_instanciated_dict', None)
+            if values_dict is not None:
+                delattr(value, '_auto_instanciated_dict')
+                if isinstance(obj, Configurable):
+                    value.parent = obj
+                    value.config = obj.config
+                    vars(value).update(values_dict)
 
-        return value
+        return super().validate(obj, value)
--- tests/test_autoinstance_traitlet.py
+++ tests/test_autoinstance_traitlet.py
@@ -6,8 +6,9 @@
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 
 import pytest
-from polyvers._vendor.traitlets.traitlets import List, CInt
+from polyvers._vendor.traitlets.traitlets import List, CInt, Int
 from polyvers._vendor.traitlets.config import Configurable, Config
+from polyvers._vendor.traitlets import default
 
 from polyvers.autoinstance_traitlet import AutoInstance
 
@@ -95,3 +96,30 @@ def test_recursive_merge():
     a = AA(config=Config(cfg))
     assert [el.b.i for el in a.aa] == [1, 11, -1, -1]
     assert [el.bb.i for el in a.aa] == [2, -2, 22, -2]
+
+
+def test_default_value():
+    class B(Configurable):
+        i = Int().tag(config=True)
+
+    class A(Configurable):
+        b = AutoInstance(B, default_value={'i': 2}).tag(config=True)
+    assert A().b.i == 2
+
+    assert A(config=Config({'A': {'b': {'i': 3}}})).b.i == 3
+
+
+def test_dynamic_default():
+    class B(Configurable):
+        i = Int().tag(config=True)
+
+    class A(Configurable):
+        b = AutoInstance(B).tag(config=True)
+
+        @default('b')
+        def _get_i(self):
+            return dict({'i': 2})
+
+    assert A().b.i == 2
+
+    assert A(config=Config({'A': {'b': {'i': 3}}})).b.i == 3
