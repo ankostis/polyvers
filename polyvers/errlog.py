e009514490b3b38eadcdde2d3d5b0d6099e1098f
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-03-25 21:17:34 +0300

    WIP-1 feat(errlog): new _ELNode class to hold errors-tree [skip ci]

--- polyvers/errlog.py
+++ polyvers/errlog.py
@@ -6,9 +6,11 @@
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 #
+from builtins import property
 """Gather multiple exceptions in a nested contexts. """
 
 from typing import Any, List, Dict, Union, Tuple, Optional, Sequence  # noqa: F401 @UnusedImport
+import contextlib
 import logging
 
 from . import cmdlets
@@ -22,17 +24,102 @@ from ._vendor.traitlets.traitlets import Bool, CBool, Int, Unicode, Instance
 log = logging.getLogger(__name__)
 
 
+class ErrLogErrors(cmdlets.CmdException):
+    def __init__(self, etuples, doing=None):
+        #self.etuples = etuples
+
+        doing = ' while %s' % doing if doing else ''
+        self.is_all_forced = all(is_forced for _, is_forced, _ in etuples)
+        reason = 'Bypassed' if self.is_all_forced else 'Collected'
+        erlines = ''.join('\n  %s' % ErrLog._format_etuple(*etuple)
+                          for etuple in etuples)
+        self.msg = "%s %i error(s)%s: %s" % (reason, len(etuples), doing, erlines)
+
+    def __str__(self):
+        return self.msg
+
+
+class _ELNode(trt.HasTraits):
+    """
+    The basic element of a *recursive* "stack" of.
+
+    ::
+
+        node :=   (doing, is_forced, exception, [node, ...])
+
+    Cannot store textualized exception in the first place because
+    this is only known after all its children have been born (if any).
+    """
+    doing = Unicode(default_value=None, allow_none=True)
+    is_forced = Bool(default_value=None, allow_none=True)
+    err = Instance(Exception, default_value=None, allow_none=True)
+    cnodes = ListTrait()
+
+    def new_cnode(self, doing, is_forced):
+        ## `is_empty` already checked by client code
+        #   to explain situation.
+        assert all(i is None
+                   for i in (self.doing, self.is_forced, self.err)
+                   ), self
+
+        self.doing = doing or ''
+        self.is_forced = bool(is_forced)
+
+        if self.cnodes is None:
+            self.cnodes = []
+
+        child = _ELNode()
+        self.cnodes.append(child)
+
+        return child
+
+    def dissarm(self, err=None):
+        self.doing = self.is_forced = self.err = err
+
+    @property
+    def is_empty(self):
+        props = (self.doing, self.is_forced, self.err)
+        is_empty = any(i is None for i in props)
+        if is_empty and not all(i is None for i in props):
+            raise ErrLogErrors('Expected an fully empty %r!' % self)
+
+        return is_empty
+
+    def cnode_coordinates(self, node, coords: List[int]):
+        if self is node:
+            return
+
+        for i, self in enumerate(self.cnodes):
+            if node is self:
+                coords.append(i)
+                return
+
+    def __repr__(self):
+        props = (self.doing, self.is_forced, self.cnodes)
+        ## Avoid recursion using `is_empty()`.
+        is_empty = any(i is None for i in props)
+        if is_empty:
+            return 'ELN()'
+        return 'ELN(%s, %s, %s, %s)@%s' % (
+            self.doing, self.is_forced, self.err,
+            self.cnodes and '...' or '[]', id(self))
+
+
+_ELNode.cnodes._trait = Instance(_ELNode)
+
+
+## TODO: decouple `force` from `ErrLog`.
 class ErrLog(cmdlets.Replaceable, trt.HasTraits):
     """
-    A contextman collecting or "forcing" errors in a :class:`Spec` error-list.
+    A contextman collecting or "forcing" in a :class:`Spec` error-list.
 
-    - Unknown errors (not in `exceptions`) always bubble up immediately.
-    - If `token` given and exitst in :attr:`Spec.force`, errors are "forced",
-      i.e. are collected and logged as `log_level` when :meth:`report_errors()`
+    - Unknown (not in `exceptions`) always bubble up immediately.
+    - If `token` given and exitst in :attr:`Spec.force`, are "forced",
+      i.e. are collected and logged as `log_level` when :meth:`report()`
       invoked.
-    - Non-"forced" errors are either `raise_immediately`, or raised collectively
-      when :meth:`report_errors()` invoked.
-    - Collected errors ("forced" or non-`raise_immediately`) are logged on DEBUG
+    - Non-"forced" are either `raise_immediately`, or raised collectively
+      when :meth:`report()` invoked.
+    - Collected ("forced" or non-`raise_immediately`) are logged on DEBUG
       immediately.
 
     :ivar spec:
@@ -43,101 +130,36 @@ class ErrLog(cmdlets.Replaceable, trt.HasTraits):
         the :attr:`force` token to respect, like :meth:`Spec.is_force()`,
         with possible values:
           - false: (default) completely ignore `force` trait
-            (errors collected are just delayed);
-          - <a string>: "force" errors if this token is in `force` trait;
-          - `True`: "force" errors if `True` is in :attr:`force``force`.
+             collected are just delayed);
+          - <a string>: "force" if this token is in `force` trait;
+          - `True`: "force" if `True` is in :attr:`force``force`.
+    :ivar doing:
+        A description of the running activity for the current stacked-context,
+        e.g. "having fun" would result roughly in::
+
+            ErrLogErrors("Collected 2 while having fun:")
+
     :ivar raise_immediately:
-        if not forced, do not wait for `report_errors()` call to raise them;
+        if not forced, do not wait for `report()` call to raise them;
         suggested use when a function decorator.  Also when --debug.
     :ivar log_level:
-        the logging level to use when just reporting errors.
-        Note that all errors are always reported immediately on DEBUG.
+        the logging level to use when just reporting.
+        Note that all are always reported immediately on DEBUG.
 
+    :ivar _my_node:
+        a list of `doing` with each nested errlog appending one more
     :ivar _enforced_error_tuples:
-        collected arrors as a list of 3-tuples (doing, raise_later, ex),
-        cleared only when :meth:`report_errors()` called.
+        collected arrors as a list of 3-tuples (doing, is_forced, ex),
+        cleared only when :meth:`report()` called.
 
     See :meth:`Spec.errlog()` for example.
     """
-    parent = Instance(cmdlets.Forceable)
-    exceptions = ListTrait(TypeTrait(Exception))
-    token = UnionTrait((Unicode(), Bool()), allow_none=True)
-    doing = Unicode(None, allow_none=True)
-    raise_immediately = CBool()
-    log_level = UnionTrait((Int(), Unicode()))
+    class ErrLogException(Exception):
+        """A pass-through for critical ErrLog, e.g. context re-enter. """
+        pass
 
-    @property
-    def plog(self) -> logging.Logger:
-        """Search log property up the `parent` hierarchy."""
-        return getattr(self.parent, 'log', log)
-
-    def is_forced(self):
-        """Try `force` in `parent` first."""
-        return getattr(self.parent, 'is_forced')(token=self.token)
-
-    def __init__(self,
-                 parent: cmdlets.Forceable,
-                 *exceptions: Exception,
-                 token: Union[bool, str, None] = None,  # Start as collecting only
-                 doing=None,
-                 raise_immediately=None,
-                 log_level=logging.WARNING
-                 ) -> None:
-        super().__init__(parent=parent, token=token, doing=doing,
-                         raise_immediately=raise_immediately,
-                         log_level=log_level)
-        if exceptions:
-            self.exceptions = exceptions
-        self._enforced_error_tuples: List[Tuple[Any, bool, Exception]] = []
-
-    def __call__(self,
-                 *exceptions: Exception,
-                 token: Union[bool, str, None] = None,
-                 doing=None,
-                 raise_immediately=None,
-                 log_level: Union[int, str] = None):
-        """Reconfigure a new errlog."""
-        changes = {}
-        fields = zip('token doing raise_immediately log_level'.split(),
-                     [token, doing, raise_immediately, log_level])
-        for k, v in fields:
-            if v is not None:
-                changes[k] = v
-        if exceptions:  # None-check futile
-            changes['exceptions'] = self.exceptions
-
-        clone = self.replace(**changes)
-        ## Share my etuples with clone.
-        clone._enforced_error_tuples = self._enforced_error_tuples
-
-        return clone
-
-    def __enter__(self):
-        """Return `self` upon entering the runtime context."""
-        return self
-
-    def __exit__(self, exctype, excinst, _exctb):
-        if exctype is not None:
-            suppress_ex = False
-            try:
-                if issubclass(exctype, tuple(self.exceptions)):
-                    is_forced = self.is_forced()
-                    if is_forced or not self.raise_immediately:
-                        #excinst = excinst.with_traceback(exctb)  Ex already has it!
-                        self._collect_error(is_forced, excinst)
-                        suppress_ex = True
-
-                return suppress_ex
-            finally:
-                if not suppress_ex:
-                    doing = self.doing
-                    self.doing = ("%s failed unexpectedly" % doing
-                                  if doing else
-                                  "failing unexpectedly")
-                    self.report_errors(no_raise=True)
-
-    @classmethod
-    def _format_etuple(cls, doing, is_forced, ex):
+    @staticmethod
+    def _format_etuple(doing, is_forced, ex):
         doing = doing and " while %s" % doing or ''
         errtype = '"forced"' if is_forced else 'delayed'
         exstr = str(ex)
@@ -147,9 +169,139 @@ class ErrLog(cmdlets.Replaceable, trt.HasTraits):
             exstr = type(ex).__name__
         return "%s error%s -> %s" % (errtype, doing, exstr)
 
+    ## TODO: weakref(ErrLog.parent), see `BaseDescriptor._property`.
+    parent = Instance(cmdlets.Forceable)
+    exceptions = ListTrait(TypeTrait(Exception))
+    token = UnionTrait((Unicode(), Bool()), allow_none=True)
+    doing = Unicode(allow_none=True)
+    raise_immediately = CBool()
+    log_level = UnionTrait((Int(), Unicode()))
+
+    #: A point in the `_root_node` to grow from.
+    _root_node: _ELNode = Instance(_ELNode)  # type: ignore
+    _my_node = _root_node
+
+    @property
+    def plog(self) -> logging.Logger:
+        """Search `log` property in `parent`, or use this module's logger."""
+        return getattr(self.parent, 'log', log)
+
+    @property
+    def pdebug(self) -> logging.Logger:
+        """Search `debug` property in `parent`, or False."""
+        return getattr(self.parent, 'debug', False)
+
+    def is_forced(self):
+        """Try `force` in `parent` first."""
+        ## TODO: decouple `force` from `ErrLog`.
+        return getattr(self.parent, 'is_forced')(token=self.token)
+
+    @property
+    def is_root(self):
+        return self._my_node is self._root_node
+
+    @property
+    def is_armed(self):
+        """Is context ``__enter__()`` currently under process? """
+        return not self._my_node.is_empty
+
+    @property
+    def _my_node_coordinates(self):
+        return self._root_node._my_node_coords(self)
+
+    def __init__(self,
+                 parent: cmdlets.Forceable,
+                 *exceptions: Exception,
+                 token: Union[bool, str, None] = None,  # Start as collecting only
+                 doing=None,
+                 raise_immediately=None,
+                 log_level=logging.WARNING
+                 ) -> None:
+        """Root created only in constructor - the rest in __call__()/__enter__()."""
+        if not isinstance(parent, cmdlets.Forceable):
+            raise trt.TraitError("Parent '%s' is not Forceable!" % parent)
+        super().__init__(parent=parent, exceptions=exceptions,
+                         token=token, doing=doing,
+                         raise_immediately=raise_immediately,
+                         log_level=log_level,
+                         )
+        self._root_node = _ELNode()
+
+    def __repr__(self):
+        return '%s(%s, coords=root=%s, root=%s)@%s)' % (
+            type(self).__name__,
+            self._my_node, self._my_node_coordinates, self._root_node,
+            id(self))
+
+    def __call__(self,
+                 *exceptions: Exception,
+                 token: Union[bool, str, None] = None,
+                 doing=None,
+                 raise_immediately=None,
+                 log_level: Union[int, str] = None):
+        """Reconfigure a new errlog on the same stack-level."""
+        changes = {}  # to gather replaced fields
+        fields = zip('token doing raise_immediately log_level'.split(),
+                     [token, doing, raise_immediately, log_level])
+        for k, v in fields:
+            if v is not None:
+                changes[k] = v
+        if exceptions:  # None-check futile
+            changes['exceptions'] = exceptions
+
+        ## Note etuples-root & children always shared (not deepcopy).
+
+        clone = self.replace(**changes)
+
+        return clone
+
+    def __enter__(self):
+        """Return `self` upon entering the runtime context."""
+        if self.is_armed:
+            raise ErrLog.ErrLogException("Cannot re-enter context!")
+
+        new_node = self._my_node.new_cnode(self.doing, self.is_forced())
+        new_errlog = self.replace(_my_node=new_node)
+
+        return new_errlog
+
+    def __exit__(self, exctype, ex, _exctb):
+        ## 3-state out flag:
+        #  - None: no exc
+        #  - False: raising
+        #  - True: collecting raised ex
+        suppress_ex = None
+        try:
+            if exctype is not None:
+                suppress_ex = False
+
+                if issubclass(exctype, ErrLog.ErrLogException):
+                    pass  # Let critical internal error bubble-up.
+
+                if issubclass(exctype, tuple(self.exceptions)):
+                    is_forced = self.is_forced()
+                    if is_forced or not self.raise_immediately:
+                        #ex = ex.with_traceback(exctb)  Ex already has it!
+                        self._collect_error(is_forced, ex)
+                        suppress_ex = True
+
+                return suppress_ex
+        finally:
+            node = self._my_node
+            if suppress_ex is False:  # exit is raising
+                doing = node.doing
+                doing = ("%s failed unexpectedly" % doing
+                         if doing else
+                         "failing unexpectedly")
+                self.report(do_raise=False, doing=doing)
+            elif not node:
+                self.report(do_raise=None)
+            else:
+                pass  ## stacked, do nothing
+
     def _collect_error(self, is_forced, error):
         #error = error.with_traceback(exctb)  Ex already has it!
-        etuple = (self.doing, is_forced, error)
+        etuple = (self._my_node[-1], is_forced, error)
 
         log = self.plog
         if log.isEnabledFor(logging.DEBUG):
@@ -158,26 +310,44 @@ class ErrLog(cmdlets.Replaceable, trt.HasTraits):
 
         self._enforced_error_tuples.append(etuple)
 
-    def _flush_errors(self) -> Optional[str]:
-        """Generate a message for all collected errors, before clearing them."""
-        if self._enforced_error_tuples:
-            etuples = self._enforced_error_tuples
-            self._enforced_error_tuples = []  # Avoid stacktrace memleaks.
-            erlines = ''.join('\n  %s' % self._format_etuple(*etuple)
-                              for etuple in etuples)
-            doing = self.doing and ' while %s' % self.doing or ''
-            all_forced = all(is_forced for _, is_forced, _ in etuples)
-            reason = 'Bypassed' if all_forced else 'Collected'
-
-            return "%s %i error(s)%s: %s" % (
-                reason, len(etuples), doing, erlines)
-
-    def report_errors(self, no_raise=False) -> str:
-        etuples = self._enforced_error_tuples  # Order with `_flush()` matters!
+    def report(self, do_raise=None, doing=None) -> str:
+        """
+        :param raise:
+            3-state bool:
+            - None: raise only if any non-forced (i.e. stack-level == 1)
+            - False: log only (i.e. in a finally block)
+            - True: raise only (i.e. stack-level > 1)
+        """
+        etuples = self._enforced_error_tuples[:]  # Order with `_flush()` matters!
         if etuples:
-            err_msg = self._flush_errors()
-            all_forced = all(is_forced for _, is_forced, _ in etuples)
-            if no_raise or all_forced:
-                self.plog.log(self.log_level, err_msg)
+            self._enforced_error_tuples.clear()  # Avoid tb-memleaks from all clones.
+
+            ex = ErrLogErrors(etuples)
+            is_all_forced = ex.is_all_forced
+            if do_raise is True or (do_raise is None and not is_all_forced):
+                raise ex from None
+
+            if do_raise is False or (do_raise is None and is_all_forced):
+                self.plog.log(self.log_level, str(ex))
             else:
-                raise cmdlets.CmdException(err_msg)
+                raise AssertionError(do_raise, is_all_forced)
+
+
+def errlogged(*errlog_args, **errlog_kw):
+    """
+    Decorate functions/methods with a :class:`ErrLog` instance.
+
+    The errlog-contextman is attached on the wrapped function/method
+    as the `errlog` attribute.
+    """
+    def decorate(func):
+        @contextlib.wraps(func)
+        def inner(forceable, *args, **kw):
+            errlog = ErrLog(forceable, *errlog_args, **errlog_kw)
+            inner.errlog = errlog
+            with errlog(*errlog_args, **errlog_kw):
+                return func(forceable, *args, **kw)
+
+        return inner
+
+    return decorate
--- tests/test_errlog.py
+++ tests/test_errlog.py
@@ -8,14 +8,21 @@
 
 from polyvers import cmdlets, errlog
 from polyvers._vendor.traitlets import traitlets as trt
+from polyvers.logconfutils import init_logging
 from tests.conftest import clearlog
 import logging
 import re
 
 import pytest
 
+import os.path as osp
+
+
+init_logging(level=logging.DEBUG, logconf_files=[])
 
 log = logging.getLogger(__name__)
+
+mydir = osp.dirname(__file__)
 
 
 @pytest.fixture
@@ -27,13 +34,34 @@ def forceable():
 
 
 def test_ErrLog(forceable):
-    erl = errlog.ErrLog(forceable, IOError, ValueError, doing='fire')
-    #TODO assert not erl._build_error_message()
+    with pytest.raises(trt.TraitError, match='not Forceable'):
+        errlog.ErrLog(object())
 
+    erl = errlog.ErrLog(forceable)
+    assert erl.token == erl.doing is None
+    assert erl.exceptions == []
+
+    erl = errlog.ErrLog(forceable, IOError, ValueError)
+    assert erl.token is None
+    assert erl.exceptions == [IOError, ValueError]
+    erl2 = erl(token='water')
+    assert erl2.token == 'water'
+    assert erl.token is None
+    assert erl.doing == erl2.doing is None
+    assert erl.exceptions == erl2.exceptions == [IOError, ValueError]
+
+    erl(doing='fine')
+
+    erl = errlog.ErrLog(forceable, IOError, doing='fine')
     with erl(token='gg') as erl2:
+        assert erl2 is not erl
         assert erl2.token == 'gg'
         assert erl.token is None
+    with erl as erl2:
+        assert erl2 is not erl  # NOT the same!
+        assert erl2.token is None
     assert erl.token is None
+    assert erl.doing == 'fine'
     assert erl.log_level is logging.WARNING
 
     ## TODO: check stacked erl messages
@@ -44,34 +72,45 @@ def test_ErrLog_non_forced_errors(caplog, forceable):
     logging.basicConfig(level=level)
     logging.getLogger().setLevel(level)
 
-    clearlog(caplog)
-
     erl = errlog.ErrLog(forceable, IOError, ValueError)
-    with erl:
-        raise IOError("Wrong!")
-    assert len(erl._enforced_error_tuples) == 1
-    with erl(doing='burning'):  # check default `token` value
-        raise IOError("Wrong!")
-    assert len(erl._enforced_error_tuples) == 2
-    assert re.search('DEBUG +Collecting delayed error', caplog.text)
 
     clearlog(caplog)
     with pytest.raises(cmdlets.CmdException,
-                       match="Collected 2 error") as ex_info:
-        erl.report_errors()
+                       match="Collected 1 error") as ex_info:
+        with erl:
+            raise IOError("Wrong!")
+    assert len(erl._enforced_error_tuples) == 0
+    assert re.search('DEBUG +Collecting delayed error', caplog.text)
     assert '"forced"' not in str(ex_info.value)
-    assert 'while burning' in str(ex_info.value)
-    assert not erl._enforced_error_tuples
+    assert 'delayed' in str(ex_info.value)
 
+    clearlog(caplog)
+    with pytest.raises(cmdlets.CmdException,
+                       match="Collected 1 error") as ex_info:
+        with erl(doing='burning'):  # check default `token` value
+            raise IOError("Wrong!")
+    assert not erl._enforced_error_tuples
+    assert 'while burning' in str(ex_info.value)
+
+
+def test_ErrLog_mixed_errors(caplog, forceable):
+    level = logging.DEBUG
+    logging.basicConfig(level=level)
+    logging.getLogger().setLevel(level)
+
+    erl = errlog.ErrLog(forceable, IOError, ValueError)
     ## Mixed case still raises
     #
     clearlog(caplog)
     forceable.force = [True]
-    with erl():  # check default `token` value
-        raise IOError("Wrong!")
-    with erl(token=True):
-        raise IOError()
-    assert len(erl._enforced_error_tuples) == 2
+    with erl:
+        erl2 = erl.stack('foo')
+        with erl2:  # check default `token` value
+            raise IOError("Wrong!")
+        with erl2(token=True):
+            raise IOError()
+        assert len(erl._enforced_error_tuples) == 2
+    assert len(erl._enforced_error_tuples) == 0
 
     clearlog(caplog)
     with pytest.raises(cmdlets.CmdException) as ex_info:
@@ -116,3 +155,30 @@ def test_ErrLog_forced_errors(caplog, forceable):
     assert 'while looting' in caplog.text
     assert not re.search("WARNING +Delayed ", caplog.text)
     assert not erl._enforced_error_tuples
+
+
+def test_ErrLog_decorator(caplog):
+    class C(cmdlets.Spec):
+        @cmdlets.errlogged(Exception, token=True)
+        def f_log(self):
+            assert self.f_log.erl
+            raise Exception
+
+        @cmdlets.errlogged(ValueError)
+        def f_raise(self):
+            assert self.f_raise.erl
+            raise ValueError
+
+    C(force=[True]).f_log()
+    assert "Collected 1 error" in caplog.text
+
+    with pytest.raises(cmdlets.CmdException, match='Collected 1 error'):
+        C().f_log()
+
+    obj = C(force=[True])
+    clearlog(caplog)
+    obj.f_log()
+    assert "Collected 1 error" in caplog.text
+
+    with pytest.raises(cmdlets.CmdException, match='Collected 1 error'):
+        obj.f_raise()
