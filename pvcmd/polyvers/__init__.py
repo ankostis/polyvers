c095eee733d767945b2a995fc55b59fdfde14bce
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-05-23 05:17:33 +0300

    chore(ver): bump polyvers-r0.0.2a9 -> 0.0.2a9.post0

--- pvcmd/polyvers/__init__.py
+++ pvcmd/polyvers/__init__.py
@@ -14,8 +14,8 @@ __all__ = ['polyversion', 'polytime']
 
 APPNAME = 'polyvers'
 
-__version__ = polyversion(APPNAME, '0.0.0')
-__updated__ = polytime()
+__version__ = '0.0.2a9.post0'
+__updated__ = '2018-05-23T05:17:33.218516'
 __title__ = APPNAME
 __summary__ = "Bump independently versions on multi-project git repos"
 __uri__ = "https://github.com/JRCSTU/polyvers"
